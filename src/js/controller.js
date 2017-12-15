33929f1182702d27f8ad9f589c84215724875ccf
Ryan Mack <ryanjmack21@gmail.com>
2017-12-15 13:27:47 -0500

    Add restart game functionality; also console.log when a player wins

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,13 +3,37 @@ let isFirstMove = true;
 
 function handleBoardClick(e) {
   const target = e.target.dataset.index;
-
   if (!target) {
     return;
   }
 
-  game.makeMove(target, 'x');
+  const isMoveSuccessful = game.makeMove(target, 'x');
+  if (!isMoveSuccessful) {
+    return;
+  }
+
+  updateBoardView();
+
+  const state = game.getGameState();
+  if (state) {
+    if (state === 'x') {
+      console.log('x won');
+    }
+    else if (state === 'o') {
+      console.log('o won');
+    }
+    else {
+      console.log('draw');
+    }
+  }
+}
+
+
+function handleButtonClick(e) {
+  game = new Board();
+  
   updateBoardView();
 }
 
 document.querySelector('.board').addEventListener('click', handleBoardClick);
+document.querySelector('.restart-button').addEventListener('click', handleButtonClick);
--- src/js/view.js
+++ src/js/view.js
@@ -5,15 +5,19 @@ function updateBoardView() {
   const board = game.getBoardCopy();
 
   spots.forEach(spot => {
-    if (!spot.innerHTML) {
-      const value = board[spot.dataset.index]
-      if (value === 'x') {
-        spot.innerHTML = `<i class="fa fa-times" aria-hidden="true"></i>`;
-      }
-      else if (value === 'o') {
-        spot.innerHTML = `<i class="fa fa-circle-o" aria-hidden="true"></i>`;
-      }
+    // look at the dataset-index of the DOM element, look up the value in the
+    // board and update the innerHTML of the spot accordingly
+    const value = board[spot.dataset.index]
+
+
+    if (value === 'x') {
+      spot.innerHTML = `<i class="fa fa-times" aria-hidden="true"></i>`;
+    }
+    else if (value === 'o') {
+      spot.innerHTML = `<i class="fa fa-circle-o" aria-hidden="true"></i>`;
+    }
+    else {
+      spot.innerHTML = '';
     }
   });
-
 }
