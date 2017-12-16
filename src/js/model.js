240c0fa26dd660ce8c99e80ee8baeb81674900f3
Ryan Mack <ryanjmack21@gmail.com>
2017-12-16 15:08:37 -0500

    Fixed bug in win conditions. Null values could have tested for win conditions

--- src/js/model.js
+++ src/js/model.js
@@ -44,8 +44,8 @@ Board.prototype.checkDiagonals = function() {
   const board = this.getBoardCopy();
   const center = board[4];
 
-  if (board[0] === center && board[8] === center
-   || board[2] === center && board[6] === center) {
+  if (center && board[0] === center && board[8] === center
+   || center && board[2] === center && board[6] === center) {
      return center;
    }
 
@@ -56,13 +56,13 @@ Board.prototype.checkDiagonals = function() {
 Board.prototype.checkRows = function() {
   const board = this.getBoardCopy();
 
-  if (board[0] === board[1] && board[1] === board[2]) {
+  if (board[0] && board[0] === board[1] && board[1] === board[2]) {
     return board[0]
   }
-  else if (board[3] === board[4] && board[4] === board[5]) {
+  else if (board[3] && board[3] === board[4] && board[4] === board[5]) {
     return board[3]
   }
-  else if (board[6] === board[7] && board[7] === board[8]) {
+  else if (board[6] && board[6] === board[7] && board[7] === board[8]) {
     return board[6]
   }
 
@@ -73,13 +73,13 @@ Board.prototype.checkRows = function() {
 Board.prototype.checkColumns = function() {
   const board = this.getBoardCopy();
 
-  if (board[0] === board[3] && board[3] === board[6]) {
+  if (board[0] && board[0] === board[3] && board[3] === board[6]) {
     return board[0]
   }
-  else if (board[1] === board[4] && board[4] === board[7]) {
+  else if (board[1] && board[1] === board[4] && board[4] === board[7]) {
     return board[1]
   }
-  else if (board[2] === board[5] && board[5] === board[8]) {
+  else if (board[2] && board[2] === board[5] && board[5] === board[8]) {
     return board[2]
   }
 
